52101b5c88821eecefa45283407b8a15cc8d1ead
Ananthi <ananthivenkadesh23@gmail.com>
2025-03-30 13:41:26 +0530

    added with run time argument

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -1,6 +1,6 @@
 public class JenkinsExample{
 public static void main(String[] args){
-System.out.println(""Hello "+args[0]+" from Jenkins"");
-  System.out.println(""Hello "+args[1]+" from Jenkins"");
+System.out.println("Hello "+args[0]+" from Jenkins");
+  System.out.println("Hello "+args[1]+" from Jenkins");
 }
 }
